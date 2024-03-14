a5bd49efa5a60f7e8ad7c8d53fa0bb22c850e101
suttonspindler <suttonspindler@gmail.com>
2024-03-13 21:27:17 -0400

    Finished overview

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -13,21 +13,27 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              MY<span className="blue"> PROFILE </span>
+              A<span className="blue"> BRIEF OVERVIEW</span>
             </h1>
             <p className="home-about-body">
-              I am a Junior pursuing a bachelor's degree in Computer Science at Northeastern University.
+              I am a <b className="blue">dedicated</b> computer science student at Northeastern University with a GPA of <b className="blue">3.9/4.0.</b>
               <br />
-              <br />I am fluent in several programming languages including
-              <i>
-                <b className="blue"> C++, Java, and Javascript </b>
-              </i>
+              <br />I excel in languages like <b className="blue">Java, JavaScript, C++, and Python,</b> and I'm adept with tools like <b className="blue">Docker, GitHub, and React.</b>
               <br />
               <br />
-              My areas of interest are&nbsp;
-              <i><b className="blue">Software Development,</b></i>{" "}
-              <i><b className="blue">Web Development (Full Stack),</b></i> and{" "}
-              <i><b className="blue">Data Science</b></i>.
+              Currently, I'm a <b className="blue">Senior Web & Software Developer</b> for <b className="blue">Northeastern Science Magazine,</b> where I've built a website and created a network builder app using <b className="blue">Node.js and Express.</b>
+              <br />
+              <br />
+              I've also worked as a <b className="blue">Teaching Assistant,</b> helping students grasp <b className="blue">object-oriented programming</b> concepts.
+              <br />
+              <br />
+              Previously, I developed a project management dashboard for <b className="blue">Northeastern Electric Racing</b> and gained retail experience at <b className="blue">Target Corporation.</b>
+              <br />
+              <br />
+              My project portfolio includes a <b className="blue">Patient Daily Health Survey</b> web app, a Unity-based horror game called <b className="blue">"Snail Game,"</b> and a water leak detection app prototype called <b className="blue">"Leaky."</b>
+              <br />
+              <br />
+              Beyond academics, I'm passionate about <b className="blue">world cinema, geography, indie music, RPG games, and comedy sketches.</b>
               <br />
               <br />
             </p>
