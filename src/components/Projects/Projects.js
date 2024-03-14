fd827c5e69c8dec18d522d80e5a2b97866c61a1f
suttonspindler <suttonspindler@gmail.com>
2024-03-13 22:02:02 -0400

    Updated projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,7 @@ import solitaire from "../../Assets/Projects/solitaire.png";
 import leaky from "../../Assets/Projects/leaky.png";
 import survey from "../../Assets/Projects/survey.png";
 import snail from "../../Assets/Projects/snail.png";
+import escape from "../../Assets/Projects/escape.png";
 
 function Projects() {
   return (
@@ -31,7 +32,8 @@ function Projects() {
             <ProjectCard
               imgPath={snail}
               title="Snail Game"
-              description="Programmed 3D horror/puzzle game in Unity with C# as entry for 2023 Jam-O-Lantern Game Jam."
+              description="Programmed 3D horror/puzzle game in Unity with C# as entry for 2023 Jam-O-Lantern Game Jam.
+              Developed in 8-member team formed within Northeastern’s Game Studio Club."
               ghLink="https://github.com/AlexMalakov/SnailUnity"
               demoLink="https://daezel.itch.io/snail-game"
             />
@@ -52,8 +54,18 @@ function Projects() {
             <ProjectCard
               imgPath={solitaire}
               title="Marble Solitaire"
-              description="Designed game containing UI using Swing to control user input and output of images in Java."
+              description="Designed game containing UI using Swing to control user input and output of images in Java.
+              Structured program using MVC design pattern for organized separation of front-end and back-end."
               ghLink="https://github.com/suttonspindler/Marble-Solitaire"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={escape}
+              title="Planet Escape"
+              description="Developed entry for the 2021 Amaze Me Game Jam (Hackathon) hosted by YoYo Games and Opera GX.  Involved developing, programming, and testing the game using JavaScript within a 2-week time limit."
+              demoLink = "https://atomicnarration.itch.io/planet-escape" 
             />
           </Col>
 
